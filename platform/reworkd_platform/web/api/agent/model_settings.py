5f61015797e0ec6c4d20d2e5c275e02d41960789
kenmy <kenmy>
2023-06-13 22:04:38 +0800

    update

--- platform/reworkd_platform/web/api/agent/model_settings.py
+++ platform/reworkd_platform/web/api/agent/model_settings.py
@@ -11,9 +11,11 @@ openai.api_base = settings.openai_api_base
 def create_model(model_settings: ModelSettings, streaming: bool = False) -> ChatOpenAI:
     return ChatOpenAI(
         client=None,  # Meta private value but mypy will complain its missing
-        openai_api_key='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',
+        openai_api_key='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',
         temperature=model_settings.temperature,
         model=model_settings.model,
         max_tokens=model_settings.max_tokens,
         streaming=streaming,
+        openai_api_base= 'https://api.openai.com/v1',
+        openai_organization='',
     )
